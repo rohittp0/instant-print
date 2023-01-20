a6f88e3293429c538211fd302ee4bc57aa06a7b7
Rohit T P <tprohit9@gmail.com>
2023-01-21 04:35:12 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,110 +1,65 @@
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpServer;
-
-import org.jetbrains.annotations.Contract;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.print.PrinterException;
-import java.io.*;
-import java.net.InetSocketAddress;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+public class Main extends WebSocketClient {
 
-import static java.net.HttpURLConnection.HTTP_OK;
-
-public class Main {
-    private final static String HOST = "0.0.0.0";
-    private final static int PORT = 8000;
-    private final static String GET = "GET";
-    private final static String POST = "POST";
-
-    private Main() throws IOException {
-        InetSocketAddress socket = new InetSocketAddress(HOST, PORT);
-        HttpServer server = HttpServer.create(socket, 0);
-        server.createContext("/", this::index);
-        server.createContext("/print", this::print);
-
-        server.setExecutor(null); // creates a default executor
-        server.start();
-
-        System.out.println("Server listening at : " + HOST + ":" + PORT);
+    public Main(URI serverURI) {
+        super(serverURI);
     }
 
-    @Contract("_, _ -> param1")
-    private @NotNull HttpExchange writeHTML(@NotNull HttpExchange exchange, @NotNull String text) throws IOException {
-        exchange.getResponseHeaders().set("Content-Type", "text/html");
-        exchange.sendResponseHeaders(HTTP_OK, text.getBytes().length);
-
-        OutputStream output = exchange.getResponseBody();
-
-        output.write(text.getBytes());
-        output.flush();
-
-        return exchange;
+    @Override
+    public void onOpen(ServerHandshake handshake) {
+        send("Hello");
+        System.out.println("opened connection");
     }
 
-    private void index(@NotNull HttpExchange exchange) throws IOException {
-        if (!GET.equals(exchange.getRequestMethod())) {
-            exchange.sendResponseHeaders(405, -1);
-            return;
-        }
-
-
-        // Read file index.html
-        File file = new File("templates/index.html");
-        String html = new String(java.nio.file.Files.readAllBytes(file.toPath()));
-
-        writeHTML(exchange, html).close();
-    }
-
-    private void print(@NotNull HttpExchange exchange) throws IOException {
-        if (!POST.equals(exchange.getRequestMethod())) {
-            exchange.sendResponseHeaders(405, -1);
-            return;
-        }
-
-        File output;
-
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody())))
-        {
-            String line = reader.readLine();
-            String[] parts = line.split("base64,");
-
-            if (parts.length != 2) {
-                writeHTML(exchange, "You call this an HTTP request🤦").close();
-                return;
-            }
-
-            String mime = parts[0].split(":")[1].split(";")[0];
-
-            if (!mime.equals("application/pdf")) {
-                writeHTML(exchange, "I don't like your MIME").close();
-                return;
-            }
-
-            // Write bytes to file output.pdf
-            output = new File("uploads/"+Math.random()+".pdf");
-            byte[] bytes = java.util.Base64.getDecoder().decode(parts[1]);
-            java.nio.file.Files.write(output.toPath(), bytes);
-        }
-
-        PrintPDF printPDF = new PrintPDF(output);
+    @Override
+    public void onMessage(String message) {
+        File output = new File("uploads/" + Math.random() + ".pdf");
+        byte[] bytes = java.util.Base64.getDecoder().decode(message);
 
         try {
-            printPDF.print();
-        } catch (PrinterException e) {
-            throw new RuntimeException(e);
+            java.nio.file.Files.write(output.toPath(), bytes);
+            new PrintPDF(output).print();
+        } catch (PrinterException | IOException e) {
+            System.out.println("Error printing file " + e.getMessage());
+            send("Error printing file " + e.getMessage());
         }
+    }
 
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        // The close codes are documented in class org.java_websocket.framing.CloseFrame
+        System.out.println(
+                "Connection closed by " + (remote ? "remote peer" : "us") + " Code: " + code + " Reason: "
+                        + reason);
 
-        writeHTML(exchange, "Printer is trying his best 🤞").close();
+        newClient();
+    }
+
+    @Override
+    public void onError(@NotNull Exception ex) {
+        ex.printStackTrace();
+        // if the error is fatal then onClose will be called additionally
+    }
+
+    private static void newClient() {
+        try {
+            new Main(new URI("wss://print.rohittp.com/ws")).connect();
+        } catch (URISyntaxException e) {
+            System.out.println("Error connecting to websocket " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-        try {
-            new Main();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        newClient();
     }
 
 }
